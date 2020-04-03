e169e64d38306f8c6075767a7274c35559555af9
Zhipeng Zhang <zhipengzhang@Zhipengs-MacBook-Pro.local>
2020-04-03 10:08:46 +0100

    create 6 test cases to test if the automata can identify only \n

---
+++ test/Task4.java
@@ -0,0 +1,62 @@
+import org.junit.Test;
+
+import st1920.automaton.RegExpMatcher;
+
+import static org.junit.Assert.*;
+
+public class Task4 {
+	
+    @Test
+    public void ManualTest1() {
+        assertTrue(
+                RegExpMatcher.matches(
+                        "\n",
+                        "$"
+                ));
+    }
+    
+    @Test
+    public void ManualTest12() {
+        assertFalse(
+                RegExpMatcher.matches(
+                        "6",
+                        "$"
+                ));
+    }
+    
+    @Test
+    public void ManualTest3() {
+        assertFalse(
+                RegExpMatcher.matches(
+                		"\n\n",
+                        "$"
+                ));
+    }
+    
+    @Test
+    public void ManualTest4() {
+        assertFalse(
+                RegExpMatcher.matches(
+                		"a",
+                        "$"
+                ));
+    }
+    
+    @Test
+    public void ManualTest5() {
+        assertTrue(
+                RegExpMatcher.matches(
+                        "",
+                        "$*"
+                ));
+    }
+    
+    @Test
+    public void ManualTest6() {
+        assertFalse(
+                RegExpMatcher.matches(
+                        "23",
+                        "$"
+                ));
+    }
+}
